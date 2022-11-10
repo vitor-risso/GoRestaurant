3b095f5618bf2dfbf1456af1dcad7f81a1844e51
Vitor Risso <vitor.risso13@gmail.com>
2022-11-09 22:30:23 -0300

    add retrofit

--- app/src/main/java/com/vitorhr/basicandroidsetup/di/AppModule.kt
+++ app/src/main/java/com/vitorhr/basicandroidsetup/di/AppModule.kt
@@ -1,7 +1,13 @@
 package com.vitorhr.basicandroidsetup.di
 
 import org.koin.dsl.module
+import retrofit2.Retrofit
 
 val appModule = module {
-
+    // Retrofit instance
+    single { Retrofit.Builder().baseUrl(BASE_URL).build() }
+    // Create instance of the service
+//    single { get<Retrofit>().create() }
 }
+
+val BASE_URL = ""
